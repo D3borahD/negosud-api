1420c46b18c909de3004a2d6f157ccf8659b11f7
Deborah <deborah.douillet@aum.bio>
2023-03-05 20:55:53 +0100

    add CORS config

--- src/main/java/fr/negosud/api/Config/SecurityConfiguration.java
+++ src/main/java/fr/negosud/api/Config/SecurityConfiguration.java
@@ -11,16 +11,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration {
+public class SecurityConfiguration implements WebMvcConfigurer {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,16 +34,17 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                //.requestMatchers("/api/v1/auth/**").permitAll()
+
                 .requestMatchers("/api/v1/auth/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/families/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/houses/**").permitAll()
                 .requestMatchers(HttpMethod.GET,"/api/v1/products/**").permitAll()
-                //.requestMatchers(HttpMethod.GET,"/api/v1/**").permitAll()
-                //.requestMatchers("api/v1/shopping-cart/**").authenticated()
+                .requestMatchers(HttpMethod.GET,"/api/v1/houses/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/api/v1/families/**").permitAll()
+                .requestMatchers("api/v1/shopping-cart/**","api/v1/addresses/**", "api/v1/customer-orders").authenticated()
+                .requestMatchers(HttpMethod.GET,"/api/v1/users/**" ).authenticated()
+                .requestMatchers(HttpMethod.PUT,"/api/v1/users/**" ).authenticated()
                 .requestMatchers(HttpMethod.DELETE,"/api/v1/users/**").authenticated()
+                .requestMatchers("/api/v1/suppliers/**","api/v1/supplier-orders/**","api/v1/families/**","api/v1/houses/**", "api/v1/products/**").hasAuthority("ADMIN")
                 .requestMatchers(HttpMethod.DELETE,"/api/v1/**").hasAuthority("ADMIN")
-                //.requestMatchers("/api/v1/suppliers/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
--- src/main/java/fr/negosud/api/NegoSudAPIApplication.java
+++ src/main/java/fr/negosud/api/NegoSudAPIApplication.java
@@ -1,26 +1,14 @@
 package fr.negosud.api;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class NegoSudAPIApplication {
-
-   /* @Autowired
-    private CustomerService customerService;*/
 
     public static void main(String[] args) {
 
         SpringApplication.run(NegoSudAPIApplication.class, args);
     }
-
-/*   @Override
-    public void run(String... args) throws Exception {
-        Iterable<Customer> customers = customerService.getCustomers();
-       customers.forEach(customer -> System.out.println(customer.getName()));
-    }*/
-
 
 }
